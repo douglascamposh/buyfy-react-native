9d8ad456e61ba3c3aa6899ad23d6eb1982b0890c
MauricioGamarraVargas <mauriciogamarravargas@gmail.com>
2021-02-08 16:57:42 -0400

    hardware button on v1

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
 import { HeaderButton } from '../components/common';
 
 class NavigationDrawerStructure extends Component {
+  componentDidMount(){
+    this.focusListener = this.props.navigationProps.addListener('didFocus', () => {
+			BackHandler.addEventListener("hardwareBackPress", this.hardwareButtonBack);
+		});
+		this.focusListener = this.props.navigationProps.addListener('didBlur', () => {	
+       BackHandler.removeEventListener("hardwareBackPress", this.hardwareButtonBack);
+		 });
+  }
+
+  componentWillUnmount() {
+		this.focusListener.remove();
+	}
+
   toggleDrawer = () => {
     this.props.navigationProps.toggleDrawer();
+  };
+
+  hardwareButtonBack = () => {
+    const stackKey = this.props.navigationProps.state.key;
+    const historyKey = stackKey[stackKey.length -1];
+    historyKey == 0 ? BackHandler.exitApp() : this.props.navigationProps.navigate('storeList');
+    return true;
   };
 
   render() {
