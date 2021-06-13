a4b4c74f4b1adcb7bfd214cfea1d5832e1b78e8c
douglas <douglas.campos@mojix.com>
2021-06-13 18:21:04 -0400

    set up dev env and fix get day

--- src/constants/Enum.js
+++ src/constants/Enum.js
@@ -18,11 +18,11 @@ export const invoiceStates = {
 };
 
 export const weekDays = {
+  0: "sunday",
   1: 'monday',
   2: 'tuesday',
   3: 'wednesday',
   4: "thursday",
   5: "friday",
   6: "saturday",
-  7: "sunday"
 };
